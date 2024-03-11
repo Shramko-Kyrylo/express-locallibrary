890eb6622c3568a8ef9533a0a4ea9feb27199835
Shramko-Kyrylo <115269298+Shramko-Kyrylo@users.noreply.github.com>
2024-03-11 11:45:15 +0200

    Update 11.03.2024

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var favicon = require("serve-favicon");
-var bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+
 
 var app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://Kyrylo:DBKyrylo123@clusterkyrylo.adsni8g.mongodb.net/local_library?retryWrites=true&w=majority";
+
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cool', usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
